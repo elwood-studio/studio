cc4a4844d87f2bda2468437d6f95833f2dd4ebfe
Travis Kuhl <travis.kuhl@gmail.com>
2023-05-26 18:22:12 -0700

    cli verison

--- packages/cli/src/libs/get-version.ts
+++ packages/cli/src/libs/get-version.ts
@@ -1,8 +1,9 @@
-import { readPackageUp } from 'read-pkg-up';
-
 import { __dirname } from './utils.ts';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { version } from '../../package.json';
+
 export async function getVersion(): Promise<string> {
-  const pkg = await readPackageUp({ cwd: __dirname(import.meta) });
-  return pkg?.packageJson.version ?? '0';
+  return version ?? '0';
 }
